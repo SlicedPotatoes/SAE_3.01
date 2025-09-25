1bbb08dadd5d302f05b9509412a0774a37c4db71
Dimitri van Steenkiste <167211948+Steenkiste@users.noreply.github.com>
2025-09-25 16:49:53 +0200

    Début des méthodes static getAbsencesStudentFiltred et getJustificationsStudentFiltred

--- src/Model/Absence.php
+++ src/Model/Absence.php
@@ -1,5 +1,6 @@
 <?php
 require_once "State.php";
+require_once("connection.php");
 class Absence {
     private $id;
     private $time;
@@ -36,47 +37,70 @@ class Absence {
     public function getStudent() { return $this->student; }
     public function getAllowedJustification() { return $this->allowedJustification; }
 
-    /*
-     * TODO: Connecter a la base de données, faire en sorte qu'elle sois paramétrique, avec systeme de filtre / trie (?)
-     *
-    */
-    public static function getAbsences() {
-        $state = State::getAbsenceStates();
 
-        return array(
-            new Absence(
-                0,
-                (new DateTime)->setDate(2025, 9, 24),
-                "1h30",
-                true,
-                $state[1],
-                "TD",
-                "R3.01",
-                "J. Vion",
-                null
-            ),
-            new Absence(
-                1,
-                (new DateTime)->setDate(2025, 9, 24),
-                "1h30",
-                true,
-                $state[2],
-                "TD",
-                "R3.01",
-                "J. Vion",
-                null
-            ),
-            new Absence(
-                2,
-                (new DateTime)->setDate(2025, 9, 24),
-                "1h30",
-                true,
-                $state[3],
-                "TD",
-                "R3.01",
-                "J. Vion",
-                null
-            )
-        );
+    static public function getAbsencesStudentFiltred ($studentId=null, $startDate=null, $endDate=null, $examen=null, $allowedJustification=null, $stateId=null)
+    {
+        global $connection;
+        $parameters = array();
+        $query = "select * from absence";
+
+        $hasStudentId = (isset($studentId));
+        $hasStartDate = (isset($startDate));
+        $hasEndDate = (isset($endDate));
+        $hasStateId = (isset($stateId));
+
+        if ($hasStudentId)
+        {
+            $parameters["studentId"] = $studentId;
+            $query .= " where idStudent = :studentId";
+        }
+
+        if ($hasStartDate)
+        {
+            $parameters["dateDebut"] = $startDate;
+            $query .= " INTERSECT select * from absence where time >= :dateDebut";
+        }
+
+        if ($hasEndDate)
+        {
+            $parameters["dateFin"] = $endDate;
+            $query .= " INTERSECT select * from absence where time <= :dateFin";
+        }
+
+        if ($examen)
+        {
+            $query .= " INTERSECT select * from absence where examen = true";
+        }
+
+        if ($allowedJustification)
+        {
+            $query .= " INTERSECT select * from absence where allowedJustification = true";
+        }
+
+        if ($hasStateId)
+        {
+            $parameters["stateId"] = $stateId;
+            $query .= " INTERSECT select * from absence where idstate = :stateId";
+        }
+
+        $request = $connection->prepare($query);
+
+        foreach ($parameters as $key => $value)
+        {
+            $request->bindParam($key, $value);
+        }
+
+        $request->execute();
+        $result = $request->fetchAll();
+
+        //
+        // TO DO : TRANSFORMER LES ABSENCES EN OBJECT DE CLASS ABSENCE
+        //
+
+
+        var_dump($result);
+        echo $query;
     }
 }
+
+Absence::getAbsencesStudentFiltred(1, '2015-01-05', '2018-01-01', true, false, 2);
--- src/Model/Justification.php
+++ src/Model/Justification.php
@@ -55,4 +55,50 @@ class Justification
     {
         return $this->justificationFile;
     }
+
+    function getJustificationsStudentFiltred ($startDate=null, $endDate=null, $examen=null, $stateId=null)
+    {
+        $studentID = $this->getStudentId();
+
+        $parameters = array("studentID" => $studentID);
+        $query = "select * from absences where idStudent = :studentID";
+
+        $hasDateDebut = (isset($startDate));
+        $hasDateFin = (isset($endDate));
+        $hasStateId = (isset($stateId));
+
+        if ($hasDateDebut)
+        {
+            $parameters["dateDebut"] = $startDate;
+            $query .= "INTERSECT select * from absences where date_debut >= :dateDebut";
+        }
+
+        if ($hasDateFin)
+        {
+            $parameters["dateFin"] = $endDate;
+            $query .= "INTERSECT select * from absences where date_fin >= :dateFin";
+        }
+
+        if ($examen)
+        {
+            $query .= "INTERSECT select * from absences where examen = true";
+        }
+
+        if ($hasStateId)
+        {
+            $parameters["stateId"] = $stateId;
+            $query .= "INTERSECT select * from absences where state_id = :stateId";
+        }
+
+        $request = $connection->prepare($query);
+
+        foreach ($parameters as $key => $value)
+        {
+            $request->bindParam($key, $value);
+        }
+
+        $request->execute();
+        $result = $request->fetch();
+        return $result;
+    }
 }
