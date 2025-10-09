21a4e085c6ce8e4d555e987fb6d196ea35a17ef2
Yann <yann.dascotte01@gmail.com>
2025-10-09 18:57:48 +0200

    Fix require / jeu de test

--- Test/testAbsenceFilter.php
+++ Test/testAbsenceFilter.php
@@ -1,5 +1,5 @@
 <?php
-require_once "src/Model/Absence.php";
+require_once __DIR__ . "/../src/Model/Absence.php";
 
 echo "<pre>";
 
@@ -8,10 +8,10 @@ $tests = [
     "Test 2 : No Student" => [999999, null, null, false, false, null],
     "Test 3 : Student good" => [null, null, null, false, false, null],
     "Test 4 : Unversed Date" => [null, '2025-10-12', '2025-10-01', false, false, null],
-    "Test 5 : endDate only" => [null, null, '2025-10-01', false, false, null],
-    "Test 6 : startDate only" => [null, '2025-10-10', null, false, false, null],
-    "Test 7 : same Date" => [null, '2025-10-10', '2025-10-10', false, false, null],
-    "Test 8 : Normal Date" => [null, '2025-10-10', '2025-10-12', false, false, null],
+    "Test 5 : endDate only" => [null, null, '2025-09-05', false, false, null],
+    "Test 6 : startDate only" => [null, '2025-09-04', null, false, false, null],
+    "Test 7 : same Date" => [null, '2025-09-01', '2025-09-01', false, false, null],
+    "Test 8 : Normal Date" => [null, '2025-09-05', '2025-09-06', false, false, null],
     "Test 9 : Examen" => [null, null, null, true, false, null],
     "Test 10 : No justification" => [null, null, null, false, true, null],
     "Test 11 : Validated" => [null, null, null, false, false, 'Validated'],
@@ -26,7 +26,7 @@ foreach ($tests as $label => [$studentId, $startDate, $endDate, $exam, $allowed,
     $result = Absence::getAbsencesStudentFiltered($studentId, $startDate, $endDate, $exam, $allowed, $state);
     echo "Résultat : " . count($result) . " absences trouvées\n";
     if (!empty($result)) {
-        print_r($result);
+        print_r($result[0]);
     }
     echo "\n";
 }
