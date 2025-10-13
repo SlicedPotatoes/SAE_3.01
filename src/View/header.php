c973b80176ac2d82ba18ce5f1457daac9059e931
Dimitri van Steenkiste <167211948+Steenkiste@users.noreply.github.com>
2025-10-13 11:47:16 +0200

    Commentaire et réorganisation de header.php et Student.php

--- src/View/header.php
+++ src/View/header.php
@@ -1,22 +1,37 @@
-<!-- Message de bienvenu -->
-<h1 class="h3">Bonjour <span class="text-uphf fw-bold"><?=$_SESSION['student']->getFirstName(), " ", $_SESSION['student']->getLastName()?></span> !</h1>
+<!--Récupération des données-->
+<?php
+$lastName = $_SESSION['student']->getLastName();
+$firstName = $_SESSION['student']->getFirstName();
+$absenceTotal = $_SESSION['student']->getAbsTotal();
+$halfdayTotal = $_SESSION['student']->getHalfdaysAbsences();
+$absenceAllowJustification = $_SESSION['student']->getAbsCanBeJustified();
+$absenceNotJustified = $_SESSION['student']->getAbsNotJustified();
+$absenceRefused = $_SESSION['student']->getAbsRefused();
+$malus = $_SESSION['student']->getMalusPoints();
+$malusWithoutPending = $_SESSION['student']->getMalusPointsWithoutPending();
+?>
+
+<!-- En tête saluant l'étudiant -->
+<h1 class="h3">Bonjour <span class="text-uphf fw-bold"><?=$firstName, " ", $lastName?></span> !</h1>
 <div class="header-line-brand-color"></div>
 
-<!-- Étudiant : Card avec information sur son assiduité -->
+<!-- Card du dashboard avec informations sur l'assiduité -->
 <div class="row row-cols-2 row-cols-md-4 g-3 mb-3">
 
+<!--    Card pour afficher les absences totals de l'étudiant-->
     <div class="col">
         <div class="card shadow-sm border-primary text-center h-100 card-compact">
             <div class="card-body">
                 <div class="card-title small mb-1">Absences totales</div>
                 <div class="fs-4 text-primary mb-0">
-                    <?= (int) $_SESSION['student']->getAbsTotal() ?>
+                    <?= (int)$absenceTotal ?>
                 </div>
 
-                <?php if ($_SESSION['student']->getAbsTotal() > 0): ?>
+<!--                Affichage des demi-journées d'absences si elles sont différentes des absences-->
+                <?php if ($absenceTotal ==! $halfdayTotal): ?>
                     <div class="text-muted small">
                         Demi-journées d’absence totales :
-                        <?= (int) $_SESSION['student']->getHalfdaysAbsences() ?>
+                        <?= (int) $halfdayTotal ?>
                     </div>
                 <?php endif; ?>
 
@@ -24,37 +39,53 @@
         </div>
     </div>
 
+<!--    Card pour afficher les absences qui peuvent être justifiée-->
     <div class="col">
         <div class="card shadow-sm border-info text-center h-100 card-compact">
             <div class="card-body">
                 <div class="card-title small mb-1">Absences à justifier</div>
                 <div class="fs-4 text-info mb-0">
-                    <?= (int) $_SESSION['student']->getAbsCanBeJustified() ?>
+                    <?= (int) $absenceAllowJustification ?>
                 </div>
             </div>
         </div>
     </div>
 
+<!--    Absences pénalisante, contribue au malus-->
     <div class="col">
         <div class="card shadow-sm  border-warning text-center h-100 card-compact">
             <div class="card-body">
                 <div class="card-title small mb-1">Absences pénalisantes</div>
                 <div class="fs-4 text-warning mb-0">
-                    <?= (int) ($_SESSION['student']->getAbsNotJustified() + $_SESSION['student']->getAbsRefused()) ?>
+                    <?= (int) ($absenceNotJustified + $absenceRefused) ?>
                 </div>
             </div>
         </div>
     </div>
 
+<!--    Malus actuel et projection du malus avec justification des absences en attente -->
     <div class="col">
-        <?php $malus = $_SESSION['student']->getMalusPoints(); ?>
+<!--        If permettant d'avoir deux affichages différents si l'étudiant à un malus ou non-->
+
+<!--        Si l'étudiant à un malus la card sera rouge -->
         <?php if ($malus > 0): ?>
             <div class="card shadow-sm border-danger text-center h-100 card-compact">
                 <div class="card-body">
                     <div class="card-title small mb-1">Malus</div>
                     <div class="fs-4 text-danger mb-0">-<?= $malus ?>&nbsp;</div>
+<!--                Si l'étudiant a des absences en attente alors le malus pourrais être réduit ou même retiré
+                    Ainsi il est important de montrer à l'étudiant l'impacte de la justification de ses absences
+                    sur le malus-->
+                <?php if ($malusWithoutPending ==! $malus): ?>
+                    <div class="text-muted small">
+                        Malus si validation des absences : -
+                        <?= $malusWithoutPending ?>
+                    </div>
+                <?php endif; ?>
                 </div>
             </div>
+
+<!--        L'étudiant sans malus a ces informations -->
         <?php else: ?>
             <div class="card shadow-sm border-success text-center h-100 card-compact">
                 <div class="card-body">
@@ -64,5 +95,4 @@
             </div>
         <?php endif; ?>
     </div>
-
 </div>
