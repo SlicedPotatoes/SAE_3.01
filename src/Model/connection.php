87011db538ecf5bd293a8ef60c3b8722a1785c51
Isaac GODISIABOIS <isaac.godisiabois@uphf.fr>
2025-10-16 10:45:04 +0200

    Refonte du système de test

--- src/Model/connection.php
+++ src/Model/connection.php
@@ -1,10 +1,20 @@
 <?php
+global $TEST;
+require "../Presentation/globalVariables.php";
 // établit une variable connexion qui fait la liaison avec la base de données
-$host = "tommytech.net";
-$user = "kevin";
-$password = "patate360";
-$dbname = "postgres";
-$port = "5432";
+if($TEST){
+    $host = "localhost";
+    $user = "postgres";
+    $password = "12345";
+    $dbname = "postgres";
+    $port = "5432";
+}else{
+    $host = "tommytech.net";
+    $user = "kevin";
+    $password = "patate360";
+    $dbname = "postgres";
+    $port = "5432";
+}
 try {
     $connection = new PDO("pgsql:host=$host;dbname=$dbname", $user, $password);
     $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
