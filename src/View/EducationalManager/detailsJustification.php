7e01d1fdd4e2e41ab228567fde330db83eddcd5a
Kevin M <53843746+SlicedPotatoes@users.noreply.github.com>
2025-11-04 01:05:46 +0100

    Fluidifier le design de Détails des justificatifs

--- src/View/EducationalManager/detailsJustification.php
+++ src/View/EducationalManager/detailsJustification.php
@@ -2,8 +2,6 @@
 // detailsJustification
 
 use Uphf\GestionAbsence\Model\Justification\Justification;
-use Uphf\GestionAbsence\Presentation\JustificationPresentation;
-use Uphf\GestionAbsence\Model\Justification\File;
 use Uphf\GestionAbsence\Model\Justification\StateJustif;
 
 
@@ -12,22 +10,18 @@ $justification = Justification::getJustificationById($idJustification);
 $absences = $justification->getAbsences();
 $files = $justification->getFiles();
 
-
 ?>
-<div class="tab-content bg-white border-bottom border-start border-end rounded-bottom pt-4 p-3" id="Liste-Abs-Justification-PR">
+<div class="card d-flex flex-column gap-3 mt-4 p-3">
+    <div>
+        <!-- Justificatif + Etat -->
+        <div class="d-flex justify-content-between align-items-center">
+            <h2 class="mb-1">Justificatif du : <?= $justification->getSendDate()->format('d/m/Y') ?></h2>
+            <span class="badge rounded-pill text-bg-<?= $justification->getCurrentState()->colorBadge() ?> fs-6 px-3 py-2">
+                <?= $justification->getCurrentState()->label() ?>
+            </span>
+        </div>
 
-
-    <!-- Justificatif + Etat -->
-    <div class="d-flex justify-content-between align-items-center mb-4 ">
-        <h2 class="mb-0">Justificatif du : <?= $justification->getSendDate()->format('d/m/Y') ?></h2>
-        <span class="badge rounded-pill text-bg-<?= $justification->getCurrentState()->colorBadge() ?> fs-6 px-3 py-2">
-        <?= $justification->getCurrentState()->label() ?>
-    </span>
-    </div>
-
-    <!-- Dates de début et fin -->
-
-    <div class="mb-4">
+        <!-- Dates de début et fin -->
         <div class="col-md-6">
             <strong>Date début :</strong> <?= $justification->getStartDate()->format('d/m/Y') ?>
         </div>
@@ -37,13 +31,12 @@ $files = $justification->getFiles();
     </div>
 
     <!-- Affichage des absences -->
-
-    <h4 class="mb-3">Absences</h4>
-    <div class="accordion accordion-flush" id="absFlush">
-        <?php if (!empty($absences)) : ?>
+    <div>
+        <h4>Absences</h4> <!-- TODO: Peut être ajouter un bouton "Tout Validé" et "Tout Refusé" -->
+        <div class="border-top">
+            <?php //for($i = 0; $i < 10; $i++): ?>
             <?php foreach ($absences as $index => $absence) : ?>
-                <?php $abs = $absence; ?>
-                <div class="accordion-item d-flex align-items-center justify-content-between p-2">
+                <div class="d-flex align-items-center justify-content-between border-bottom py-2">
                     <div class="flex-grow-1 me-3">
                         <div class="d-flex flex-column">
                             <div>Date: <?= $absence->getTime()->format('d/m/Y H:i') ?></div>
@@ -52,45 +45,43 @@ $files = $justification->getFiles();
                     </div>
                     <?php if ($justification->getCurrentState() === StateJustif::NotProcessed) : ?>
                         <div class="form-check form-switch">
-                            <input type="hidden" 
+                            <input type="hidden"
                                    form="validateJustificationForm"
-                                   name="absences[<?= $absence->getIdAccount() ?>_<?= $absence->getTime()->format('Y-m-d H:i:s') ?>]" 
-                                   value="validated" 
+                                   name="absences[<?= $absence->getIdAccount() ?>_<?= $absence->getTime()->format('Y-m-d H:i:s') ?>]"
+                                   value="validated"
                                    class="absence-state">
                             <button type="button" class="btn btn-success absence-btn">Validé</button>
                         </div>
                     <?php endif; ?>
                 </div>
             <?php endforeach; ?>
-        <?php endif; ?>
+            <?php //endfor ?>
+        </div>
     </div>
-</div>
 
-
-<div class="tab-content bg-white border-bottom border-start border-end rounded-bottom pt-4 mt-4 p-3" id="Details-Justification-Abs-PR">
+    <!-- Information remplie par l'étudiant pour le justificatif -->
     <div class="row">
         <div class="col-md-6">
-            <h4 class="mb-3">Motif de l'absence:</h4>
-            <h6><?= $justification->getCause() ?></h6>
+            <h4>Motif de l'absence:</h4>
+            <p><?= htmlspecialchars($justification->getCause()) ?></p>
         </div>
+        <!-- Liste des fichiers -->
         <div class="col-md-6">
-            <h4 class="mb-3">Justificatifs:</h4>
-            <ul class="list-group mb-3">
+            <h4>Justificatifs:</h4>
+            <ul class="list-group">
                 <?php foreach ($files as $file): ?>
-                    <li class="list-group-item d-flex justify-content-between align-items-center">
-                        <?= htmlspecialchars($file->getFileName()) ?>
-                        <a href="uploads/<?= htmlspecialchars($file->getFileName()) ?>" target="_blank" class="btn btn-sm btn-primary">
-                            Télécharger
-                        </a>
+                    <li class="list-group-item d-flex justify-content-between align-items-center pe-2">
+                        <span title="<?= htmlspecialchars($file->getFileName()) ?>" class="text-truncate"><?= htmlspecialchars($file->getFileName()) ?></span>
+
+                        <div class="btn-group btn-group-sm">
+                            <button class="btn btn-outline-danger bi bi-eye me-1" title="Voir" type="button"></button>
+                            <a class="btn btn-outline-primary bi bi-download" title="Télécharger" href="" download="raw (1).png"></a>
+                        </div>
                     </li>
                 <?php endforeach; ?>
             </ul>
         </div>
     </div>
-
-
-
-
 
     <!-- Justificatif état non traité côté RP -->
     <?php if ($justification->getCurrentState() === StateJustif::NotProcessed
@@ -98,7 +89,7 @@ $files = $justification->getFiles();
         <form id="validateJustificationForm" action="./Presentation/validateJustification.php" method="post">
             <input type="hidden" name="idJustification" value="<?= $justification->getIdJustification() ?>">
 
-            <label for="JustificationRejectionReason" class="form-label h5 mb-2">Raison du Refus</label>
+            <label for="JustificationRejectionReason" class="form-label h4 mb-2">Raison du Refus</label>
             <!-- TODO: Si absence coché donc refuser => apparation textArea sinon hidden ( JS )  -->
             <textarea name="rejectionReason" id="JustificationRejectionReason" class="form-control" rows="5"></textarea>
 
@@ -108,7 +99,7 @@ $files = $justification->getFiles();
             </div>
         </form>
 
-        <!-- Justificatif état traité côté RP-->
+    <!-- Justificatif état traité côté RP-->
     <?php elseif ($justification->getCurrentState() === StateJustif::Processed) : ?>
         <?php if (!empty($justification->getRefusalReason())) : ?>
             <div class="alert alert-warning mt-4">
@@ -123,9 +114,8 @@ $files = $justification->getFiles();
             <a href="index.php?currPage=dashboard" class="btn btn-secondary">Retour</a>
         </div>
     <?php endif; ?>
+
 </div>
-
-
 
 <script>
     document.querySelectorAll('.absence-btn').forEach(button => {
