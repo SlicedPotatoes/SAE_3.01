155257aa0d670938c5afcb5e0c19804a5aee6199
Louis <plouvierlouis@gmail.com>
2025-11-25 15:05:17 +0100

    Add teacher home page and controller for absence management

---
+++ src/Controller/TeacherHomeController.php
@@ -0,0 +1,23 @@
+<?php
+namespace Uphf\GestionAbsence\Controller;
+
+
+use Uphf\GestionAbsence\Model\AuthManager;
+use Uphf\GestionAbsence\Model\DB\Select\StudentSelector;
+use Uphf\GestionAbsence\Model\Entity\Account\AccountType;
+use Uphf\GestionAbsence\Model\Notification\Notification;
+use Uphf\GestionAbsence\Model\Notification\NotificationType;
+
+class TeacherHomeController {
+
+//    public static function show(array $params): ControllerData
+//    {
+//        // Utilisateur non connecté, rediriger vers /
+//        if (!AuthManager::isLogin()) {
+//            header("Location: /");
+//            exit();
+//        }
+//
+//    }
+
+}
