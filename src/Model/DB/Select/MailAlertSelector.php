a8c2da7c37d4f28a96b3378fc510014ba232eb41
Isaac <godisiabois.isaac@gmail.com>
2026-01-06 22:23:06 +0100

    Fonction de récupération du bool de MailAlertSelector.php

---
+++ src/Model/DB/Select/MailAlertSelector.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Uphf\GestionAbsence\Model\DB\Select;
+require "../Connection.php";
+require "../../../../vendor/autoload.php";
+
+use Uphf\GestionAbsence\Model\DB\Connection;
+
+class MailAlertSelector
+{
+    public static function MailAlertSIsActivated(int $idAccount):array
+    {
+        $pdo = Connection::getInstance();
+        $query = $pdo->prepare('SELECT mailAlertTeacher.activated as mailAlertTeacher, mailAlertEducationalManager.activated as mailAlertEducationalMannager FROM account join mailAlertTeacher using (idaccount) left join mailAlertEducationalManager using (idaccount) WHERE account.idAccount = :idAccount;');
+        $query->bindValue(':idAccount', $idAccount);
+        $query->execute();
+        $result = $query->fetch();
+        return $result;
+    }
+}
+var_dump(MailAlertSelector::MailAlertSIsActivated(1));
