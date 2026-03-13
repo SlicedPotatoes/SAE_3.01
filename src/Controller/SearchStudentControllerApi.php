dbb15bd83fbb3c174cb218e842892a485002a24d
Kevin M <53843746+SlicedPotatoes@users.noreply.github.com>
2026-03-13 04:12:47 +0100

    SearchStudent -> Validation + utilisation dans SearchStudentControllerApi

--- src/Controller/SearchStudentControllerApi.php
+++ src/Controller/SearchStudentControllerApi.php
@@ -2,11 +2,14 @@
 
 namespace Uphf\GestionAbsence\Controller;
 
+use Respect\Validation\Exceptions\NestedValidationException;
 use Uphf\GestionAbsence\Model\Entity\Account\Student;
-use Uphf\GestionAbsence\Model\Validation\SearchStudentValidator;
+use Uphf\GestionAbsence\Model\Notification\Notification;
+use Uphf\GestionAbsence\Model\Notification\NotificationType;
 use Uphf\GestionAbsence\Service\AccountService;
 use Uphf\GestionAbsence\Utils\ResponseApi\HttpStatus;
 use Uphf\GestionAbsence\Utils\ResponseApi\ResponseApi;
+use Uphf\GestionAbsence\Validator\SearchStudentValidator;
 
 /**
  * Controller Rest pour la recherche d'étudiant côté Responsable pédagogique
@@ -18,11 +21,19 @@ class SearchStudentControllerApi
      * Renvoi un JSON des étudiants trouvé avec les filtres, permet ainsi leurs affichages via Ajax JavaScript
      */
     public static function getSearchStudent(): void {
-        $validator = new SearchStudentValidator($_GET);
-        $filters = $validator->getData();
+        try {
+            SearchStudentValidator::validationPostPredefinedComment($_GET);
+            $students = Student::jsonSerializeStudent(AccountService::getFilteredStudents($_GET));
 
-        $students = Student::jsonSerializeStudent(AccountService::getFilteredStudents($filters));
+            new ResponseApi(HttpStatus::OK, $students)->done();
+        }
+        catch (NestedValidationException $e) {
+            foreach ($e->getMessages() as $message) {
+                Notification::addNotification(NotificationType::Error, $message);
+            }
 
-        new ResponseApi(HttpStatus::OK, ["result" => $students])->done();
+            new ResponseApi(HttpStatus::BAD_REQUEST)->done();
+        }
+
     }
 }
