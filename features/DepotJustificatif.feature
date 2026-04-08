8a02b9cfaab374e8abe483dc43ebe4c7db3c18b1
Isaac GODISIABOIS <isaac.godisiabois@uphf.fr>
2026-04-08 13:58:54 +0200

    rework FeatureContext.php

--- features/DepotJustificatif.feature
+++ features/DepotJustificatif.feature
@@ -5,22 +5,22 @@ Feature: Depot Justificatif
 
   Scenario:
     Given je suis connecté à un compte étudiant de numéro étudiant "22400227" sur la page de dépot de justificatif
-    And j ai une absence le "04/04/2026" à "8H00" d une durée de "1h30" et "sans" examen
-    And j ai une absence le "04/06/2026" à "8H00" d une durée de "1h30" et "sans" examen
-    And j ai une absence le "04/07/2026" à "8H00" d une durée de "1h30" et "sans" examen
+    And j ai une absence le "04/04/2026" à "8H00" d une durée de "1H30" et "sans" examen
+    And j ai une absence le "04/06/2026" à "8H00" d une durée de "1H30" et "sans" examen
+    And j ai une absence le "04/07/2026" à "8H00" d une durée de "1H30" et "sans" examen
     When je met en date de départ "2026-04-01" et en date de fin "2026-04-30"
     And j ai entré un commentaire qui dit "Malade"
     And j appuie sur le bouton envoyer le justificatif
     Then le nombre de justificatif doit être égale à "1"
-    And l'absence du "04/04/2026" à "8H00" d une durée de "1h30" et "sans" examen doit être en "Pending"
-    And l'absence du "04/06/2026" à "8H00" d une durée de "1h30" et "sans" examen doit être en "NotJustified"
-    And l'absence du "04/07/2026" à "8H00" d une durée de "1h30" et "sans" examen doit être en "NotJustified"
+    And l absence du "04/04/2026" à "8H00" d une durée de "1h30" et "sans" examen doit être en "Pending"
+    And l absence du "04/06/2026" à "8H00" d une durée de "1h30" et "sans" examen doit être en "NotJustified"
+    And l absence du "04/07/2026" à "8H00" d une durée de "1h30" et "sans" examen doit être en "NotJustified"
 
   Scenario:
     Given je suis connecté à un compte étudiant de numéro étudiant "22400227" sur la page de dépot de justificatif
     When je met en date de départ "2026-05-01" et en date de fin "2026-05-30"
     And j ai entré un commentaire qui dit "Malade"
-    And j'appuie sur le bouton envoyer le justificatif
+    And j appuie sur le bouton envoyer le justificatif
     Then le nombre de justificatif doit être égale à "0"
 
   Scenario Outline:
@@ -30,9 +30,9 @@ Feature: Depot Justificatif
     And j ai entré un commentaire qui dit <com>
     And j appuie sur le bouton envoyer le justificatif
     Then le nombre de justificatif doit être égale à <nbJusti>
-    And l'absence du <date> à <heure> d une durée de <duree> et <exam> examen doit être en <etat>
+    And l absence du <date> à <heure> d une durée de <duree> et <exam> examen doit être en <etat>
     Examples:
       | etu      | date         | heure  | duree  | exam   | debut        | fin          | com      | nbJusti | etat           |
-      | 22400227 | "04/01/2026" | "8H00" | "1h30" | "sans" | 20260101     | "2026-01-30" | "Malade" | 0       | "NotJustified" |
-      | 22400227 | "04/02/2026" | "8H00" | "1h30" | "sans" | "2026-02-01" | "2026-02-28" | ""       | 0       | "NotJustified" |
-      | 22400227 | "04/03/2026" | "8H00" | "1h30" | "sans" | "2026-03-30" | "2026-03-01" | "Malade" | 0       | "NotJustified" |
+      | 22400227 | "04/01/2026" | "8H00" | "1H30" | "sans" | 20260101     | "2026-01-30" | "Malade" | 0       | "NotJustified" |
+      | 22400227 | "04/02/2026" | "8H00" | "1H30" | "sans" | "2026-02-01" | "2026-02-28" | ""       | 0       | "NotJustified" |
+      | 22400227 | "04/03/2026" | "8H00" | "1H30" | "sans" | "2026-03-30" | "2026-03-01" | "Malade" | 0       | "NotJustified" |
--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -1,29 +1,23 @@
 <?php
 
+use Behat\Behat\Context\Context;
 use Dotenv\Dotenv;
 use Uphf\GestionAbsence\Database\Select\SelectBuilder\JustificationSelectBuilder;
-use Behat\Behat\Context\Context;
-use Behat\Gherkin\Node\PyStringNode;
-use Behat\Gherkin\Node\TableNode;
-use Behat\Step\Given;
-use Behat\Step\When;
-use Behat\Step\Then;
 use PHPUnit\Framework\TestCase;
-use Uphf\GestionAbsence\Database\Select\StudentSelector;
-use Uphf\GestionAbsence\Model\Entity\Absence\Absence;
-use Uphf\GestionAbsence\Model\Entity\Absence\Resource;
 use Uphf\GestionAbsence\Model\Entity\Absence\CourseType;
 use Uphf\GestionAbsence\Model\Entity\Absence\StateAbs;
 use Uphf\GestionAbsence\Database\Insert\AbsenceInsertor;
 use Uphf\GestionAbsence\Service\JustificationService;
 use Uphf\GestionAbsence\Database\Select\SelectBuilder\AbsenceSelectBuilder;
 
-class FeatureContext extends BehatContext
+class FeatureContext implements Context
 {
     private $start;
     private $end;
     private $etu;
     private $commentaire;
+    private $exeption;
+
     /**
      * @Given /^je suis connecté à un compte étudiant de numéro étudiant "([^"]*)" sur la page de dépot de justificatif$/
      */
@@ -37,20 +31,20 @@ class FeatureContext extends BehatContext
      */
     public function jAiUneAbsenceLeÀDUneDuréeDeEtExamen($arg1, $arg2, $arg3, $arg4)
     {
-        $dotenv = Dotenv::createImmutable(dirname(__DIR__,2) , '/.env.test');
+        $dotenv = Dotenv::createImmutable(dirname(__DIR__, 2), '/.env.test');
         $dotenv->load();
-        if($arg4 === "sans") {
+        if ($arg4 === "sans") {
             $exam = "Non";
-        }else{
-            $exam= "Oui";
+        } else {
+            $exam = "Oui";
         }
-        AbsenceInsertor::addAbsences([["Identifiant" => $this->etu,"Date"=>$arg1,"Heure"=>$arg2,
-            "Durée"=>$arg3,"Type"=>CourseType::BEN,"Matière"=>"Ressource 1","Groupes"=>"BUT INFO 2 Groupe A1",
-            "Profs"=>"","Contrôle"=>$exam, "Absent/Présent"=>"Absence"]]);
+        AbsenceInsertor::addAbsences([["Identifiant" => $this->etu, "Date" => $arg1, "Heure" => $arg2,
+            "Durée" => $arg3, "Type" => CourseType::BEN, "Matière" => "Ressource 1", "Groupes" => "BUT INFO 2 Groupe A1",
+            "Profs" => "", "Contrôle" => $exam, "Absent/Présent" => "Absence"]]);
     }
 
     /**
-     * @When /^je met en date de départ "2026\-04\-01"([^"]*)"2026\-04\-30"$/
+     * @When /^je met en date de départ "([^"]*)" et en date de fin "([^"]*)"$/
      */
     public function jeMetEnDateDeDépartEtEnDateDeFin($arg1, $arg2)
     {
@@ -71,11 +65,15 @@ class FeatureContext extends BehatContext
      */
     public function jAppuieSurLeBoutonEnvoyerLeJustificatif()
     {
-        $data = [];
-        $data['absenceReason'] = $this->commentaire;
-        $data['startDate'] = $this->start;
-        $data['endDate'] = $this->end;
-        JustificationService::addJustification(1, $data, []);
+        try {
+            $data = [];
+            $data['absenceReason'] = $this->commentaire;
+            $data['startDate'] = $this->start;
+            $data['endDate'] = $this->end;
+            JustificationService::addJustification(1, $data, []);
+        }catch (\Exception $e){
+            $this->exeption = $e;
+        }
     }
 
     /**
@@ -83,16 +81,23 @@ class FeatureContext extends BehatContext
      */
     public function leNombreDeJustificatifDoitÊtreÉgaleÀ($arg1)
     {
-        $justsifications = new JustificationSelectBuilder()->dateStart($this->start." 08:00:00")->dateEnd($this->end." 08:00:00")->execute();
-        TestCase::assertEquals($arg1,count($justsifications));
+        $justsifications = new JustificationSelectBuilder()->dateStart($this->start . " 08:00:00")->dateEnd($this->end . " 08:00:00")->execute();
+        TestCase::assertEquals($arg1, count($justsifications));
     }
 
     /**
-     * @Given /^l'absence du "([^"]*)" à "([^"]*)" d une durée de "([^"]*)" et "([^"]*)" examen doit être en "([^"]*)"$/
+     * @Given /^l absence du "([^"]*)" à "([^"]*)" d une durée de "([^"]*)" et "([^"]*)" examen doit être en "([^"]*)"$/
      */
     public function lAbsenceDuÀDUneDuréeDeEtExamenDoitÊtreEn($arg1, $arg2, $arg3, $arg4, $arg5)
     {
-        $abs = new AbsenceSelectBuilder()->dateStart($this->start." 08:00:00")->dateEnd($this->end." 08:00:00")->execute();
-        TestCase::assertEquals(StateAbs::from($arg5),$abs[0]->getCurrentState());
+        // Crée un objet DateTime à partir du format français
+        $dateObj = DateTime::createFromFormat('d/m/Y', $arg1);
+
+        // Retourne la date au format souhaité
+        $dateObj->format('Y-m-d');
+
+        $abs = new AbsenceSelectBuilder()->dateStart($dateObj->format('Y-m-d') . " 08:00:00")->
+        dateEnd($dateObj->format('Y-m-d') . " 08:00:00")->execute();
+        TestCase::assertEquals(StateAbs::from($arg5), $abs[0]->getCurrentState());
     }
 }
